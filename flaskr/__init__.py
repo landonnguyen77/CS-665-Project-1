6f1492f8be93b7e860da9a3398f44f7802c55699
Landon Nguyen <landonnguyen77@gmail.com>
2025-11-18 20:18:11 -0600

    initial flask setup

---
+++ flaskr/__init__.py
@@ -0,0 +1,29 @@
+import os 
+from flask import Flask
+
+def create_app(test_confige=None):
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+    )
+
+    if test_confige is None:
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        app.config.from_mapping(test_confige)
+
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    @app.route('/')
+    def index():
+        return 'Welcome to Flaskr!'
+
+    @app.route('/hello')
+    def hello():
+        return 'Hello, World!'
+    return app
+    
