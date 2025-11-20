cbc7981415263cb61e43008f5222401bbef68b31
Landon Nguyen <landonnguyen77@gmail.com>
2025-11-19 19:10:48 -0600

    add database initialization functions

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,5 +1,6 @@
-import os 
+import os
 from flask import Flask
+from . import db
 
 def create_app(test_confige=None):
     app = Flask(__name__, instance_relative_config=True)
@@ -25,5 +26,8 @@ def create_app(test_confige=None):
     @app.route('/hello')
     def hello():
         return 'Hello, World!'
+
+    db.init_app(app)
+
     return app
     
